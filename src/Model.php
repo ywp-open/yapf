fa89dc4a360e8728ea17c9aadb1243b0004b5fad
apeng <wp.y01@qq.com>
2020-07-28 21:13:40 +0800

    refactor model access method

--- src/Model.php
+++ src/Model.php
@@ -3,77 +3,72 @@ namespace Yapf;
 use Yapf\DB as db;
 
 abstract class Model{
-    private string $table;
-    private array $field = array();
+  private array $field = array();
+  private static array $dao = array();
 
-    protected function __construct(string $table){
-        $this->table = $table;
+  public static function dao():self {
+    $md5_str = md5(static::class);
+    if(!array_key_exists($md5_str,self::$dao)){
+      $dao = new static();
+      self::$dao[$md5_str] = $dao;
+      return $dao;
     }
+    return self::$dao[$md5_str];
+  }
 
-    public static function dao():self {
-        if(!property_exists(static::class,'dao')){
-            throw new \Exception(static::class.' missing variable protected static $dao');
-        }
-        if(is_null(static::$dao)){
-            static::$dao = new static();
-            return static::$dao;
-        }
-        return static::$dao;
-    }
+  public function set($name,$value):self {
+    $this->field[$name] = $value;
+    return $this;
+  }
 
-    public function set($name,$value):self {
-        $this->field[$name] = $value;
-        return $this;
+  public function save():int {
+    if(!$this->field){
+        throw new \Exception('nothing data');
     }
+    return db::dao()->insert(static::$table,$this->field)->rowCount();
+  }
 
-    public function save():int {
-        if(!$this->field){
-            throw new \Exception('nothing data');
-        }
-        return db::dao()->insert($this->table,$this->field)->rowCount();
+  public function update(array $where):int {
+    if(!$this->field){
+        throw new \Exception('nothing data');
     }
+    return db::dao()->update(static::$table,$this->field,$where)->rowCount();
+  }
 
-    public function update(array $where):int {
-        if(!$this->field){
-            throw new \Exception('nothing data');
-        }
-        return db::dao()->update($this->table,$this->field,$where)->rowCount();
+  public function findAll(string $where='',array $field=array(),string $order='id desc'):array {
+    $sql = 'select ';
+    if($field){
+        $sql .= implode(',',$field);
     }
+    else{
+        $sql .= '*';
+    }
+    $sql .= ' from '.static::$table;
+    if($where){
+        $sql .= ' where '.$where;
+    }
+    $sql .= ' order by '.$order;
+    return db::findAll($sql);
+  }
 
-    public function findAll(string $where='',array $field=array(),string $order='id desc'):array {
-        $sql = 'select ';
-        if($field){
-            $sql .= implode(',',$field);
-        }
-        else{
-            $sql .= '*';
-        }
-        $sql .= ' from '.$this->table;
-        if($where){
-            $sql .= ' where '.$where;
-        }
-        $sql .= ' order by '.$order;
-        return db::findAll($sql);
+  public function findFirst(string $where='',array $field=array()):?array {
+    $sql = 'select ';
+    if($field){
+        $sql .= implode(',',$field);
     }
+    else{
+        $sql .= '*';
+    }
+    $sql .= ' from ' . static::$table;
+    if($where){
+        $sql .= ' where ' . $where;
+    }
+    $data = db::findFirst($sql);
+    return $data;
+  }
 
-    public function findFirst(string $where='',array $field=array()):?array {
-        $sql = 'select ';
-        if($field){
-            $sql .= implode(',',$field);
-        }
-        else{
-            $sql .= '*';
-        }
-        $sql .= ' from ' . $this->table;
-        if($where){
-            $sql .= ' where ' . $where;
-        }
-        $data = db::findFirst($sql);
-        return $data;
-    }
-
-    public function delete(array $where):int {
-        $ret = db::dao()->delete($this->table,$where);
-        return $ret->rowCount();
-    }
+  public function delete(array $where):int {
+    $ret = db::dao()->delete(static::$table,$where);
+    return $ret->rowCount();
+  }
 }
